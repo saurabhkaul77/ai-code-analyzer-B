ac81cfae4fd829b6159755c7cd92d18ef8314909
saurabhkaul77 <77skaul@gmail.com>
2025-10-29 01:14:59 +0530

    change in  B

--- src/app.js
+++ src/app.js
@@ -5,13 +5,9 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
-
 app.get('/',  (req, res)=> {
     res.send('Hello World');
 })
-
 app.use('/ai', aiRoutes);
-
 module.exports = app;
